e742d174bbe54e679dbb766bae230939c07537bf
Toni Mitrevski <toni@stativa.com.mk>
2018-10-21 23:54:14 +0200

    include templates

--- microservice1/models.py
+++ microservice1/models.py
@@ -20,3 +20,6 @@ class Pet(models.Model):
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- microservice1/views.py
+++ microservice1/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse, Http404
+from .models import Pet
 
-# Create your views here.
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
+
+
+def pet_detail(request, id):
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404(f"Pet Not Found {id}")
+    return render(request, 'pet_detail.html', {'pet': pet})
+
+
+def all_pets(request):
+    pet = Pet.objects.all().values()
+    return JsonResponse(list(pet), safe=False)
